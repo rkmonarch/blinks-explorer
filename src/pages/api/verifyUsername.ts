6bd2a8ad3faf890c6855f3ba45d6ba2e0f8df8ef
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-10 20:45:27 +0530

    feat: verify username API

---
+++ src/pages/api/verifyUsername.ts
@@ -0,0 +1,23 @@
+import prisma from "@/utils/prisma-client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    const {username} = req.body;
+    try {
+        const existingUsername = await prisma.User.findFirst({
+            where: {
+                username: username,
+            },
+        });
+
+        if (existingUsername) {
+            return res.status(400).json({ isTaken: true });
+        }   
+        else {
+            return res.status(200).json({ isTaken: false });
+        }     
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
