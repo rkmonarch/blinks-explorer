443ad333e4f03872180c231dff4cc86cb8a0586c
Harsh <harshgajjar145@gmail.com>
2024-07-15 20:20:58 +0530

    fea: share

--- src/components/cards/BlinkCard.tsx
+++ src/components/cards/BlinkCard.tsx
@@ -1,10 +1,12 @@
-'use client';
+"use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import useBlink from '@/hooks/useBlink';
-import useBlinkStore from '@/store/blinks';
-import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import useBlink from "@/hooks/useBlink";
+import useRegistry from "@/hooks/useRegistry";
+import useBlinkStore from "@/store/blinks";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export type BlinkCardProps = {
   blink: string;
@@ -16,52 +18,59 @@ export type BlinkCardProps = {
 export default function BlinkCard(props: BlinkCardProps) {
   const { fetchBlink } = useBlink();
   const { setCurrentBlink } = useBlinkStore();
+  const route = useRouter();
+  const { verifyBlink } = useRegistry();
   const { data: blink } = useQuery({
-    queryKey: ['blink', props.blink],
+    queryKey: ["blink", props.blink],
     queryFn: ({ queryKey }) => fetchBlink(queryKey[1]),
   });
-  const route = useRouter();
+  const { data: verified } = useQuery({
+    queryKey: ["verified", props.website],
+    queryFn: ({ queryKey }) => verifyBlink(queryKey[1]),
+  });
+
+  console.log(verified);
 
   return (
-    <div className='relative  break-inside-avoid group mb-7 w-full'>
-      {/* <div className='absolute hidden group-hover:flex  items-end justify-between w-full p-3 transition-all'>
-        <div className='bg-gray-400 bg-opacity-35 p-1 px-3 rounded-2xl'>
-          <div className='text-xs bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>
-            Verified
-          </div>
-        </div>
-        <div>go</div>
-      </div> */}
-
+    <div className="relative  break-inside-avoid group mb-7 w-full">
       <img
         onClick={() => {
-          setCurrentBlink(blink!, props.blink, props.avatar, props.username);
-          route.push('/blinkpage');
+          setCurrentBlink(
+            blink!,
+            props.blink,
+            props.avatar,
+            props.username,
+            verified || false
+          );
+          route.push("/blinkpage");
         }}
         src={blink?.icon}
-        alt=''
-        className='w-full  border border-black border-opacity-10 bg-white rounded-xl cursor-pointer'
-        style={{ width: '100%', height: 'auto' }}
+        alt=""
+        className="w-full  border border-black border-opacity-10 bg-white rounded-xl cursor-pointer"
+        style={{ width: "100%", height: "auto" }}
       />
 
-      <div className='flex items-center justify-between mt-1'>
-        <div className='flex items-center gap-2'>
-          <Avatar className='w-4 h-4 sm:w-6 sm:h-6'>
+      <div className="flex items-center justify-between mt-1">
+        <div className="flex items-center gap-2">
+          <Avatar className="w-4 h-4 sm:w-6 sm:h-6">
             <AvatarImage
               src={
                 props.avatar === null
-                  ? 'https://github.com/shadcn.png'
+                  ? "https://github.com/shadcn.png"
                   : props.avatar
               }
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <p className='text-sm font-medium font-inter'>{props.username}</p>
+          <p className="text-sm font-medium font-inter">{props.username}</p>
         </div>
-        <div className='cursor-pointer hover:text-blue-500 text-gray-400 flex items-center gap-1'>
-          <p className='text-xs font-normal font-inter'>{props.website}</p>
-          {/* <LinkIcon width={16} height={16} color='#B5B5B5' /> */}
-        </div>
+        <Link
+          className="cursor-pointer hover:text-blue-500 text-gray-400 flex items-center gap-1 text-xs font-normal font-inter"
+          href={`https://${props.website}`}
+          target="_blank"
+        >
+          {props.website}
+        </Link>
       </div>
     </div>
   );
--- src/store/blinks.ts
+++ src/store/blinks.ts
@@ -9,16 +9,17 @@ export interface IBlinkStore {
     website: string;
     avatar: string;
     username: string;
+    verified: boolean,
   } | null;
-  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string) => void;
+  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string, verified: boolean) => void;
   setStoreBlinks: (blink: StoredBlinks) => void;
 }
 
 const useBlinkStore = create<IBlinkStore>((set) => ({
   currentBlink: null,
-  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string) => set({ currentBlink: { blink, website, avatar, username }}),
+  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string, verified: boolean) => set({ currentBlink: { blink, website, avatar, username, verified } }),
   storeBlinks: [],
-  setStoreBlinks: (blink: StoredBlinks) => set({ storeBlinks: blink}),
+  setStoreBlinks: (blink: StoredBlinks) => set({ storeBlinks: blink }),
 }));
 
 export default useBlinkStore;
