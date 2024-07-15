15345556d1e2926b2fdefd554a49875583eae4f1
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-15 17:09:32 +0530

    feat: single blink page

--- src/store/blinks.ts
+++ src/store/blinks.ts
@@ -1,14 +1,25 @@
+import { Blink } from "@/types/blink";
 import { StoredBlinks } from "@/types/storedBlinks";
 import { create } from "zustand";
 
 export interface IBlinkStore {
   storeBlinks: StoredBlinks;
+  currentBlink: {
+    blink: Blink;
+    website: string;
+    avatar: string;
+    username: string;
+  } | null;
+  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string) => void;
   setStoreBlinks: (blink: StoredBlinks) => void;
 }
 
 const useBlinkStore = create<IBlinkStore>((set) => ({
+  currentBlink: null,
+  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string) => set({ currentBlink: { blink, website, avatar, username }}),
   storeBlinks: [],
   setStoreBlinks: (blink: StoredBlinks) => set({ storeBlinks: blink}),
 }));
 
 export default useBlinkStore;
+
