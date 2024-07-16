8817ecbb8c1dbc8e1b90948616ad477f83458cf8
Harsh <harshgajjar145@gmail.com>
2024-07-16 19:32:44 +0530

    fixes

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -11,7 +11,6 @@ import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import MultipleSelector from "../../components/ui/multiple-selector";
 import { useRouter } from "next/navigation";
-// import mixpanel from "@/utils/mixpanel";
 
 interface Action {
   pathPattern: string;
