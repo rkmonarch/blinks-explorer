1dfe7414ca7abc497bbd3c218b6ffcf53e39af10
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-21 20:24:50 +0530

    fixes: pagingations

--- src/hooks/useBlinks.ts
+++ src/hooks/useBlinks.ts
@@ -1,41 +1,54 @@
-import useBlinkStore from '@/store/blinks';
-import useSearchStore from '@/store/search';
-import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react'
+import useBlinkStore from "@/store/blinks";
+import useSearchStore from "@/store/search";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
 
 export default function useBlinks() {
-    const [selectedTag, setSelectedTag] = useState<string>("");
-    const { setStoreBlinks, page, setTotalPage } = useBlinkStore()
-    const { setFilteredBlinks } = useSearchStore()
+  const [selectedTag, setSelectedTag] = useState<string>("");
+  const { setStoreBlinks, page, setTotalPage } = useBlinkStore();
+  const { setFilteredBlinks } = useSearchStore();
+  const [hasMore, setHasMore] = useState(true);
 
-    async function getBlinks() {
-        const response = await fetch("/api/get-blinks", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(selectedTag ? {
-                tags: [selectedTag],
-                page: page,
-                limit: 15
-            } : {
-                tags: [],
-                page: page,
-                limit: 15
-            }),
-        });
-        const blinks = await response.json();
-        setStoreBlinks(blinks.data);
-        setFilteredBlinks(blinks.data)
-        setTotalPage(blinks.totalPages)
-        return blinks;
-    }
-
-    const { data, isLoading, isError, refetch } = useQuery({
-        queryKey: ["blinkURL", selectedTag],
-        queryFn: getBlinks,
-        enabled: !!selectedTag || selectedTag === "",
+  async function getBlinks() {
+    const response = await fetch("/api/get-blinks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        selectedTag
+          ? {
+              tags: [selectedTag],
+              page: page,
+              limit: 15,
+            }
+          : {
+              tags: [],
+              page: page,
+              limit: 15,
+            }
+      ),
     });
+    const blinks = await response.json();
+    setStoreBlinks(blinks.data);
+    setFilteredBlinks(blinks.data);
+    setTotalPage(blinks.totalPages);
+    return blinks;
+  }
 
-    return { isError, isLoading, refetch, selectedTag, setSelectedTag }
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["blinkURL", selectedTag],
+    queryFn: getBlinks,
+    enabled: !!selectedTag || selectedTag === "",
+  });
+
+  return {
+    isError,
+    isLoading,
+    refetch,
+    selectedTag,
+    setSelectedTag,
+    hasMore,
+    setHasMore,
+  };
 }
--- src/store/blinks.ts
+++ src/store/blinks.ts
@@ -4,31 +4,42 @@ import { create } from "zustand";
 
 export interface IBlinkStore {
   storeBlinks: StoredBlinks;
-  page: number,
+  page: number;
   currentBlink: {
     blink: Blink;
     website: string;
     avatar: string;
     username: string;
-    verified: boolean,
+    verified: boolean;
   } | null;
-  totalPage: number
+  totalPage: number;
   setPage: (page: number) => void;
-  setTotalPage: (totalPage: number) => void
-  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string, verified: boolean) => void;
+  setTotalPage: (totalPage: number) => void;
+  setCurrentBlink: (
+    blink: Blink,
+    website: string,
+    avatar: string,
+    username: string,
+    verified: boolean
+  ) => void;
   setStoreBlinks: (blink: StoredBlinks) => void;
 }
 
 const useBlinkStore = create<IBlinkStore>((set) => ({
   currentBlink: null,
   page: 1,
-  totalPage: 1,
-  setCurrentBlink: (blink: Blink, website: string, avatar: string, username: string, verified: boolean) => set({ currentBlink: { blink, website, avatar, username, verified } }),
+  totalPage: 0,
+  setCurrentBlink: (
+    blink: Blink,
+    website: string,
+    avatar: string,
+    username: string,
+    verified: boolean
+  ) => set({ currentBlink: { blink, website, avatar, username, verified } }),
   storeBlinks: [],
   setPage: (page) => set({ page }),
   setStoreBlinks: (blink) => set({ storeBlinks: blink }),
-  setTotalPage: (totalPage) => set({ totalPage })
+  setTotalPage: (totalPage) => set({ totalPage }),
 }));
 
 export default useBlinkStore;
-
