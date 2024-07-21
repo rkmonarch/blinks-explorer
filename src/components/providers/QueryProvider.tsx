c43a86a540f02cf5e23c1c6b4123274ed0352cd9
Harsh <harshgajjar145@gmail.com>
2024-07-21 14:46:07 +0530

    fix: query window reload

--- src/components/providers/QueryProvider.tsx
+++ src/components/providers/QueryProvider.tsx
@@ -8,7 +8,13 @@ export default function QueryProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
